a1cfa3b3847c6a9edad8b96dd64f5ba09450bbfa
den-m1985 <denismartyanov244@gmail.com>
2023-04-09 20:43:28 +0200

    added toys to array

---
+++ toys/ToysToArray.java
@@ -0,0 +1,36 @@
+package toys;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ToysToArray<Toy> {
+    private final ArrayList<Toy> toysArray;
+
+
+    public ToysToArray(ArrayList<Toy> toysArray) {
+        this.toysArray = toysArray;
+    }
+
+
+    public void addToy(Toy toy) {
+        toysArray.add(toy);
+    }
+
+
+    public Toy getRandomToy() {
+        if (this.toysArray.isEmpty()) {
+            return null;
+        } else {
+            Random random = new Random();
+            int i = random.nextInt(toysArray.size());
+            return toysArray.get(i);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Toys:\n" + toysArray;
+    }
+
+}
