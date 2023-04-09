8eaac2ca30f662c42a19e19970423ec4e4b445e2
den-m1985 <denismartyanov244@gmail.com>
2023-04-09 20:45:09 +0200

    added toys factory

---
+++ factory/ToysFactory.java
@@ -0,0 +1,31 @@
+package factory;
+
+import toys.Toy;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ToysFactory {
+
+    /**
+     * Create snakes.
+     */
+    public static ArrayList<Toy> createToys(int toyCount) {
+        Random random = new Random();
+        ArrayList<Toy> createToys = new ArrayList<>();
+
+        for (int i = 0; i < toyCount; i++) {
+
+            int rand1 = random.nextInt(1, 100);  // имя
+            String name = "Игрушка " + rand1;
+            int rand2 = random.nextInt(1, 50);   // кол-во ????
+            int rand3 = random.nextInt(1, 99);   // вес
+            Toy toy = new Toy(name, rand2, rand3);
+            toy.setId(i+1);
+
+            createToys.add(toy);
+        }
+        return createToys;
+    }
+
+}
